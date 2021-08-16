7ab7ad2006b236dafeb040f998fa831b73eaa1ba
Joandy Jean-Baptiste <71527954+Joandyjb@users.noreply.github.com>
2021-08-16 19:02:44 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Die from './Die';
+import './Die.css';
+import RollDice from './RollDice';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Die face="five"/>
+      <Die face="four"/>
+      <Die face="three"/>
+      <Die face="five"/>
+      <Die face="one"/>
+      <Die face="two"/> */}
+    < RollDice />
     </div>
   );
 }
